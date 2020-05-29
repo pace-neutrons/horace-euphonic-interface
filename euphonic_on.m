90cf2254c2fa5c859bd8cd1faa9de8b27677034b
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2020-05-29 13:23:01 +0100

    Update euphonic_on
     - Remove loading with pyversion - when a Matlab function is
       called containing py.*, python is already loaded anyway
     - Remove adding euphonic_sf to path - euphonic_on must be
       added to the path anyway so should already be on the path
     - Add importlib check for Euphonic
     - Add check for euphonic_sf function

--- euphonic_on.m
+++ euphonic_on.m
@@ -1,31 +1,23 @@
-function euphonic_on(varargin)
-% Do required setup to enable calling euphonic_sf as a Horace disp2sqw
-% simulation function
-if nargin > 0 && varargin{1} ~= ""
-    pyexe_path = varargin{1};
-else
-    pyexe_path = '/bin/python';
-end
-if nargin > 1
-    euphonic_sf_path = varargin{2};
-else
-    euphonic_sf_path = 'horace-euphonic-interface';
-end
-
-% Add euphonic_sf.m to path
-addpath([euphonic_sf_path])
-
-% Set number of openblas threads to 1 so it doesn't interfere with
-% openmp
-setenv('OPENBLAS_NUM_THREADS', '1');
-
-% Load Python, and issue a warning if it has already been loaded
+function euphonic_on()
+% Check if Euphonic can be loaded
 try
-    pyversion pyexe_path;
+    py.importlib.import_module('euphonic');
 catch ME
-    warning(['Couldn''t load Python at %s, has euphonic_on already been ' ...
-             'called in this session?\n%s'], pyexe_path, ME.message);
+    [ver, ex, isloaded] = pyversion;
+    warning(['Couldn''t import Euphonic Python library. Does the ' ...
+             'currently loaded Python at %s have Euphonic installed?\n%s'], ...
+             ex, ME.message);
 end
+
+exists = exist('euphonic_sf');
+if exists ~= 2
+    warning(['The euphonic_sf function doesn''t exist on the current ' ...
+             'MATLAB path or isn''t the expected type (exist returned %d). '...
+             'Has euphonic_sf been added to the path?'], exists);
+end
+
+% Set number of openblas threads to 1 so it doesn't interfere with openmp
+setenv('OPENBLAS_NUM_THREADS', '1');
 
 if ~ispc
     %  For running on Linux only - avoids incompatible compile time option
