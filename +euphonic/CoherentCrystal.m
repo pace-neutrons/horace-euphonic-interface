56a0fe0b6ae7ca48bdd2ad7f3acddf7a675344ff
Duc Le <duc.le@stfc.ac.uk>
2021-02-19 01:30:07 +0000

    Add matlab chunking

--- +euphonic/CoherentCrystal.m
+++ +euphonic/CoherentCrystal.m
@@ -17,6 +17,33 @@ classdef CoherentCrystal < light_python_wrapper.light_python_wrapper
                 obj.pyobj = py.euphonic_horace.CoherentCrystal(args{:});
                 obj.populate_props();
             end
+            obj.overrides = {'horace_disp'};
+        end
+        function out = horace_disp(self, qh, qk, ql, pars, varargin)
+            % Overrides Python function to do chunking in Matlab to print messages.
+            horace_disp = py.getattr(self.pyobj, 'horace_disp');
+            chunk_size = double(self.pyobj.chunk);
+            lqh = numel(qh);
+            if self.pyobj.verbose && chunk_size > 0
+                self.pyobj.chunk = 0;
+                nchunk = ceil(lqh / chunk_size);
+                pyout = {};
+                for ii = 1:nchunk
+                    qi = (ii-1)*chunk_size + 1;
+                    qf = min([ii*chunk_size lqh]);
+                    fprintf('Using Euphonic to interpolate for q-points %d:%d out of %d\n', qi, qf, lqh);
+                    pyout = cat(1, pyout, light_python_wrapper.p2m(horace_disp(qh(qi:qf), qk(qi:qf), ql(qi:qf), pars, varargin{:})));
+                end
+                self.pyobj.chunk = chunk_size;
+                for jj = 1:2
+                    tmp = cat(1, pyout{:,jj});
+                    for ii = 1:size(tmp,2)
+                        out{jj}{ii} = cell2mat(tmp(:,ii)'); %#ok<AGROW>
+                    end
+                end
+            else
+                out = light_python_wrapper.p2m(horace_disp(qh, qh, qk, pars, varargin{:}));
+            end
         end
     end
 end
