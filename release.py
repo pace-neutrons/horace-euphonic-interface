fc562556be7cd2e6cf96a0e13b611f3b41db9732
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2021-04-20 13:12:58 +0100

    Add --create-toolbox argument to release.py
    So this argument can be called by the build process to create the
    toolbox, this should be separate from any Git release logic

--- release.py
+++ release.py
@@ -14,8 +14,16 @@ __version__ = versioneer.get_version()
 def main():
     parser = get_parser()
     args = parser.parse_args()
-
     print(args)
+
+    if args.github:
+        args.create_toolbox = True
+
+    if args.create_toolbox:
+        update_dependencies.pull_light_wrapper()
+        update_dependencies.pull_euphonic_sqw_models()
+        create_mltbx()
+
     test = not args.notest
     if args.github:
         release_github(test)
@@ -33,6 +41,26 @@ def check_submodule_version(submodule):
         raise Exception(f'Submodule {submodule} is not a tagged (release) '
                         f'version. A release version of Horace-Euphonic-Interface '
                         f'should depend on release versions of its submodules')
+
+
+def create_mltbx():
+    import fileinput
+    # replace version string
+    version = __version__.split('+')[0] if '+' in __version__ else __version__  # Matlab only accepts numbers
+    with fileinput.FileInput('mltbx/horace_euphonic_interface.prj', inplace=True) as prj:
+        for line in prj:
+            # FileInput redirect stdout to the file, for inplace replacement; end='' means don't add extra newlines
+            print(line.replace('<param.version>1.0</param.version>', f'<param.version>{version}</param.version>'), end='')
+    euphonic_version.update_euphonic_version()
+    # shutil.copytree expects destination to not exist
+    for dest_folder in ['+light_python_wrapper', 'euphonic_sqw_models', '+euphonic']:
+        if os.path.isdir('mltbx/' + dest_folder): shutil.rmtree('mltbx/' + dest_folder)
+    shutil.copytree('light_python_wrapper/+light_python_wrapper', 'mltbx/+light_python_wrapper')
+    shutil.copytree('euphonic_sqw_models/euphonic_sqw_models', 'mltbx/euphonic_sqw_models/euphonic_sqw_models')
+    shutil.copytree('+euphonic', 'mltbx/+euphonic')
+    subprocess.run(['matlab', '-batch', 'create_mltbx'], cwd='mltbx')
+    print('.mltbx created')
+
 
 def release_github(test=True):
     submodules = ['light_python_wrapper', 'euphonic_sqw_models']
@@ -68,13 +96,8 @@ def release_github(test=True):
             headers={"Authorization": "token " + os.environ["GITHUB_TOKEN"]})
         print(response.text)
 
-    # Create a Matlab toolbox and upload it
-    update_dependencies.pull_light_wrapper()
-    update_dependencies.pull_euphonic_sqw_models()
-    create_mltbx()
-    if test:
-        print("Would upload mltx to github.")
-    else:
+    # Upload Matlab toolbox
+    if not test:
         upload_url = response.json().get('upload_url')
         response = requests.post(
             upload_url,
@@ -88,6 +111,11 @@ def release_github(test=True):
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument(
+        '--create-toolbox',
+        action='store_true',
+        help=('Create .mltbx file. This is automatically set to True if '
+              '--github is used'))
+    parser.add_argument(
         '--github',
         action='store_true',
         help='Release on Github')
@@ -97,23 +125,6 @@ def get_parser():
         help='Actually send/upload')
     return parser
 
-
-def create_mltbx():
-    import fileinput
-    # replace version string
-    version = __version__.split('+')[0] if '+' in __version__ else __version__  # Matlab only accepts numbers
-    with fileinput.FileInput('mltbx/horace_euphonic_interface.prj', inplace=True) as prj:
-        for line in prj:
-            # FileInput redirect stdout to the file, for inplace replacement; end='' means don't add extra newlines
-            print(line.replace('<param.version>1.0</param.version>', f'<param.version>{version}</param.version>'), end='')
-    euphonic_version.update_euphonic_version()
-    # shutil.copytree expects destination to not exist
-    for dest_folder in ['+light_python_wrapper', 'euphonic_sqw_models', '+euphonic']:
-        if os.path.isdir('mltbx/' + dest_folder): shutil.rmtree('mltbx/' + dest_folder)
-    shutil.copytree('light_python_wrapper/+light_python_wrapper', 'mltbx/+light_python_wrapper')
-    shutil.copytree('euphonic_sqw_models/euphonic_sqw_models', 'mltbx/euphonic_sqw_models/euphonic_sqw_models')
-    shutil.copytree('+euphonic', 'mltbx/+euphonic')
-    subprocess.run(['matlab', '-batch', 'create_mltbx'], cwd='mltbx')
 
 
 if __name__ == '__main__':
