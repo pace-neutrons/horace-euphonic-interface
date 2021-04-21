798ec230729c4446c41f6a35705613331f01d4da
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2021-04-21 15:42:22 +0100

    Use urllib instead of requests
    Requests isn't in the standard library

--- update_dependencies.py
+++ update_dependencies.py
@@ -1,17 +1,17 @@
 import os
-import io
 import zipfile
-import requests
 import shutil
 import subprocess
+from urllib.request import urlretrieve
 
 
 def pull_light_wrapper():
     # Checks if the light_python_wrapper submodule has been fetched. If not, get it from github
     if not os.path.isfile('light_python_wrapper/+light_python_wrapper/light_python_wrapper.m'):
-        gh_zip = 'https://github.com/pace-neutrons/light_python_wrapper/archive/master.zip'
-        zipdata = requests.get(gh_zip, stream=True)
-        zf = zipfile.ZipFile(io.BytesIO(zipdata.content))
+        zip_name = 'master.zip'
+        gh_zip = 'https://github.com/pace-neutrons/light_python_wrapper/archive/' + zip_name
+        urlretrieve(gh_zip, zip_name)
+        zf = zipfile.ZipFile(zip_name)
         zf.extractall('.')
         rtfolder = zf.infolist()[0].filename
         shutil.copytree(rtfolder+'/+light_python_wrapper', 'light_python_wrapper/+light_python_wrapper')
@@ -20,10 +20,10 @@ def pull_light_wrapper():
 def pull_euphonic_sqw_models():
     # Checks if the euphonic_sqw_models submodule has been fetched. If not, get it from github
     if not os.path.isfile('euphonic_sqw_models/euphonic_sqw_models/euphonic_wrapper.py'):
-        import zipfile, io
-        gh_zip = 'https://github.com/pace-neutrons/euphonic_sqw_models/archive/main.zip'
-        zipdata = requests.get(gh_zip, stream=True)
-        zf = zipfile.ZipFile(io.BytesIO(zipdata.content))
+        zip_name = 'main.zip'
+        gh_zip = 'https://github.com/pace-neutrons/euphonic_sqw_models/archive/' + zip_name
+        urlretrieve(gh_zip, zip_name)
+        zf = zipfile.ZipFile(zip_name)
         zf.extractall('.')
         rtfolder = zf.infolist()[0].filename
         shutil.copytree(rtfolder+'/euphonic_sqw_models', 'euphonic_sqw_models/euphonic_sqw_models')
