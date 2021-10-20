cce5bc1730ad6dd9e96b8df6ebbb0395eb2f6d4b
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2021-10-20 14:30:33 +0100

    Fix Windows builds - use shared powershell scripts
    commit 642f7cd3bafe18e0c2a2c9377b0c169af125ddf5
    Author: Rebecca Fair <rebecca.fair@stfc.ac.uk>
    Date:   Wed Oct 20 14:14:55 2021 +0100
    
        Update PACE-jenkins-shared-library branch to main
    
    commit c4571f584d84f3bc2a2c0e08d5553df78a6f8637
    Author: Rebecca Fair <rebecca.fair@stfc.ac.uk>
    Date:   Wed Oct 20 11:44:57 2021 +0100
    
        Move startup.m logic to run_tests
    
    commit 4f80b449ffb9bcea24aa36e77b43c911db11d00d
    Author: Rebecca Fair <rebecca.fair@stfc.ac.uk>
    Date:   Wed Oct 20 11:39:06 2021 +0100
    
        Update Python setup on Linux
    
    commit 3a2cfdbe9682023f8facceecc929e3a78d080e51
    Author: Rebecca Fair <rebecca.fair@stfc.ac.uk>
    Date:   Wed Oct 20 11:23:20 2021 +0100
    
        Update Python env name on Linux
    
    commit b5aad17252885bca41b21b034296065cbe9857f4
    Author: Rebecca Fair <rebecca.fair@stfc.ac.uk>
    Date:   Wed Oct 20 11:12:58 2021 +0100
    
        Update PYTHON_EX_PATH for Linux
    
    commit b62c7aaf01688a43554901e81f8dd6a6110d8190
    Author: Rebecca Fair <rebecca.fair@stfc.ac.uk>
    Date:   Wed Oct 20 11:03:25 2021 +0100
    
        Run from test dir
    
    commit 2423315e1e8ad62f8bf4a758dc7128139e111add
    Author: Rebecca Fair <rebecca.fair@stfc.ac.uk>
    Date:   Wed Oct 20 11:01:38 2021 +0100
    
        Update toolbox creation
    
    commit 3b7a1fcb03a938e55979bf39afc7f9b079323415
    Author: Rebecca Fair <rebecca.fair@stfc.ac.uk>
    Date:   Wed Oct 20 10:54:58 2021 +0100
    
        Correctly set Python from Matlab
    
    commit 9519cf57059fe4f87b523e9e9f27135e1fd6eca2
    Author: Rebecca Fair <rebecca.fair@stfc.ac.uk>
    Date:   Wed Oct 20 10:48:26 2021 +0100
    
        Update powershell import
    
    commit d7e5295db5d690515e0fcb75afcb1678e03fc363
    Author: Rebecca Fair <rebecca.fair@stfc.ac.uk>
    Date:   Wed Oct 20 10:34:45 2021 +0100
    
        Run correct matlab script
    
    commit 03daad8925effea7a3225329e7963361fff33f31
    Author: Rebecca Fair <rebecca.fair@stfc.ac.uk>
    Date:   Wed Oct 20 10:26:46 2021 +0100
    
        Correctly set environment variables (incl. Matlab version)
    
    commit 2f6d0894d024579eede9d60b76d3ed231904d8c6
    Author: Rebecca Fair <rebecca.fair@stfc.ac.uk>
    Date:   Wed Oct 20 09:23:43 2021 +0100
    
        Use powershell to call Matlab
    
    commit 3da0a1f92a52fac7d06f3e0104a16c6ad09dbd3f
    Author: Rebecca Fair <rebecca.fair@stfc.ac.uk>
    Date:   Tue Oct 19 17:19:56 2021 +0100
    
        Use shared library powershell scripts

--- test/run_tests.m
+++ test/run_tests.m
@@ -1,4 +1,30 @@
+% Set up Python
+py_ex_path = getenv('PYTHON_EX_PATH');
+disp(py_ex_path)
+try
+  % Pyenv only introduced in 2019b
+  pyenv('Version', py_ex_path)
+catch ME
+  if (strcmp(ME.identifier,'MATLAB:UndefinedFunction'))
+    % Pyversion is deprecated but use if pyenv not available
+    pyversion(py_ex_path)
+  else
+    rethrow(ME);
+  end
+end
+
+% Add horace-euphonic-interface to Path
+addpath('..')
+addpath('../light_python_wrapper')
+
+% Set flags on Linux to avoid segfault with libraries
+if ~ispc
+  py.sys.setdlopenflags(int32(10))
+end
+
+% Ensure test data from euphonic_sqw_models is present
 verify_test_data();
+
 % Updates the required euphonic versions
 curdir = split(fileparts(mfilename('fullpath')), filesep);
 repodir = char(join(curdir(1:end-1), filesep));
