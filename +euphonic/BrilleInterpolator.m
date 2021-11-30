6143cc8b64c814602720116cb821848dc481c601
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2021-11-30 13:28:19 +0000

    Update BrilleInterpolator wrapper to work with new from_force_constants

--- +euphonic/BrilleInterpolator.m
+++ +euphonic/BrilleInterpolator.m
@@ -11,13 +11,23 @@ classdef BrilleInterpolator < light_python_wrapper.light_python_wrapper
         function obj = BrilleInterpolator(varargin)
             euphonic_on();
             light_python_wrapper.light_python_wrapper.redirect_python_warnings();
-            obj.eu = py.importlib.import_module('euphonic');
+            obj.eu = py.importlib.import_module('euphonic.brille_interpolator');
             obj.helpref = obj.eu.BrilleInterpolator;
             if ~isempty(varargin)
-                args = light_python_wrapper.light_python_wrapper.parse_args(varargin, py.getattr(obj.eu.BrilleInterpolator, '__init__'));
-                obj.pyobj = py.euphonic.BrilleInterpolator(args{:});
+                args = light_python_wrapper.light_python_wrapper.parse_args(...
+                    varargin, py.getattr(obj.eu.BrilleInterpolator, '__init__'));
+                obj.pyobj = py.euphonic.brille_interpolator.BrilleInterpolator(args{:});
                 obj.populate_props();
             end
+        end
+    end
+    methods(Static)
+        function obj = from_force_constants(varargin)
+            obj = euphonic.BrilleInterpolator;
+            args = light_python_wrapper.light_python_wrapper.parse_args(...
+                varargin, obj.eu.BrilleInterpolator.from_force_constants);
+            obj.pyobj = obj.eu.BrilleInterpolator.from_force_constants(args{:});
+            obj.populate_props();
         end
     end
 end
