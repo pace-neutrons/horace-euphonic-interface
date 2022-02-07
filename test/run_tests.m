ccbc1243e2324c2827da0ebf1ff67e4fb6bfb904
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2022-02-07 09:04:11 +0000

    Remove unnecessary Python setup
    As mltbx is installed, this will set the Python path
    correctly. Also use correct directory in runtests call.

--- test/run_tests.m
+++ test/run_tests.m
@@ -33,15 +33,9 @@ end
 % Ensure test data from euphonic_sqw_models is present
 verify_test_data();
 
-% Updates the required euphonic versions
-curdir = split(fileparts(mfilename('fullpath')), filesep);
-repodir = char(join(curdir(1:end-1), filesep));
-disp(['Adding ', repodir, ' to Python path']);
-append(py.sys.path, repodir);
-py.euphonic_version.update_euphonic_version();
-
-res = runtests("test/", 'Tag', 'integration');
-res2 = runtests("test/", 'Tag', 'help');
+% Run tests
+res = runtests(pwd, 'Tag', 'integration');
+res2 = runtests(pwd, 'Tag', 'help');
 passed = [res.Passed res2.Passed];
 if ~all(passed)
     quit(1);
