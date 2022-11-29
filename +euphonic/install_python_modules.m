74d316ce73a8f340d60d716caa651ad2b89ee733
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2022-11-29 15:38:33 +0000

    Fix install_python_modules on Windows/Python3.8+
    py.sys.executable in this case returns the MATLAB executable and
    not the Python executable! Use pyenv().Executable instead. Also
    slightly improve display/feedback to user.

--- +euphonic/install_python_modules.m
+++ +euphonic/install_python_modules.m
@@ -6,26 +6,34 @@ function install_python_modules()
     % >>  euphonic.install_python_modules
 
     req_mods = required_modules;
-    try
-        verify_python_modules(req_mods{:});
-    catch ME
-        % Checks we have the right Python version (>=3.7)
-        if strcmp(ME.identifier, 'euphonic_horace_interface:verify_python_modules:pythonVersion')
-            rethrow(ME);
-        end
-    end
+
     pipe = py.subprocess.PIPE;
     kwargs = pyargs('stdout', pipe, 'stderr', pipe);
     mods_kw = {};
     for ii = 1:2:numel(req_mods)
         mods_kw = [mods_kw req_mods{ii}];
     end
-    out = py.subprocess.run([{py.sys.executable '-m' 'pip' 'install'} mods_kw], kwargs);
+    % Get Python execuable
+    try
+        % Pyenv only introduced in 2019b
+        py_exe = pyenv().Executable;
+    catch ME
+        if (strcmp(ME.identifier,'MATLAB:UndefinedFunction'))
+            % Pyversion is deprecated but use if pyenv not available
+            [ver, py_exe, isloaded] = pyversion(py_ex_path);
+        else
+            rethrow(ME);
+        end
+    end
+    cmd = [{char(py_exe) '-m' 'pip' 'install'} mods_kw];
+    disp(strjoin(cmd));
+    out = py.subprocess.run(cmd, kwargs);
     if out.returncode ~= 0
         err = sprintf(char(uint8(out.stderr)));
         error(err);
     else
         fprintf(char(uint8(out.stdout)));
+        verify_python_modules(req_mods{:});
         disp('Successfully installed Python modules');
     end
 end
