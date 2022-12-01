db9fa35a8d70d0435f0bc6656d8f6fec4a15ecbb
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2022-12-01 10:24:44 +0000

    Use '>=' in pip install

--- +euphonic/install_python_modules.m
+++ +euphonic/install_python_modules.m
@@ -9,10 +9,7 @@ function install_python_modules()
 
     pipe = py.subprocess.PIPE;
     kwargs = pyargs('stdout', pipe, 'stderr', pipe);
-    mods_kw = {};
-    for ii = 1:2:numel(req_mods)
-        mods_kw = [mods_kw req_mods{ii}];
-    end
+    mods_kw = mod_str(req_mods);
     % Get Python execuable
     try
         % Pyenv only introduced in 2019b
@@ -25,14 +22,14 @@ function install_python_modules()
             rethrow(ME);
         end
     end
-    cmd = [{char(py_exe) '-m' 'pip' 'install'} mods_kw];
-    disp(strjoin(cmd));
+    cmd = strjoin([{char(py_exe) '-m' 'pip' 'install'} mods_kw]);
+    disp(cmd);
     out = py.subprocess.run(cmd, kwargs);
     if out.returncode ~= 0
         err = sprintf(char(uint8(out.stderr)));
         error(err);
     else
-        fprintf(char(uint8(out.stdout)));
+        disp(char(uint8(out.stdout)));
         verify_python_modules(req_mods{:});
         disp('Successfully installed Python modules');
     end
